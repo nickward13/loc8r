b21a9787a548f2af8743fa167626a9ff0162dd0c
Nick Ward <nickward@microsoft.com>
2017-05-03 20:11:10 +1000

    Moved data into controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -31,10 +31,68 @@ module.exports.locationList = function(req, res){
 
 /* GET location info page */
 module.exports.locationInfo = function(req, res){
-    res.render('location-info', { title: 'Location info' });
+    res.render('location-info', { 
+        title: 'Loc8r - Starcups',
+        pageHeader: {
+             title: 'Starcups'
+        },
+        sidebar: {
+            context: "is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.",
+            callToAction: "If you've been and you like it - or if you don't - please leave a review to help other people just like you."
+        },
+        location: {
+             name: 'Starcups',
+             address: '125 High Street, Reading, RG6 1PS',
+             openingHours: [
+                 {
+                     days: 'Monday - Friday',
+                     closed: false,
+                     opening: '7:00am',
+                     closing: '7:00pm'
+                 },
+                 {
+                     days: 'Saturday',
+                     closed: false,
+                     opening: '8:00am',
+                     closing: '4:00pm'
+                 },
+                 {
+                     days: 'Sunday',
+                     closed: true
+                 }
+             ],
+             coords: {
+                 lat: 51.455041,
+                 lng: -0.9690884
+             },
+             rating: 3,
+             facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+             distance: '100m',
+             notes: "Starcups ",
+             reviews: [
+                 {
+                     rating: 5,
+                     author: 'Simon Holmes',
+                     timestamp: '16 July 2013',
+                     text: "What a great place! I can't say enough good things about it"
+                 },
+                 {
+                     rating: 3,
+                     author: 'Charlie Chaplin',
+                     timestamp: '16 June 2013',
+                     text: "It was okay. Coffee wasn't great, but the wifi was fast."
+                 }
+             ]
+        }
+    });
 };
 
 /* GET add review page */
 module.exports.addReview = function(req, res){
-    res.render('location-review-form', { title: 'Add review' });
+    res.render('location-review-form', { 
+        title: 'Review Starcups on Loc8r',
+        pageHeader: {
+            title: 'Review Starcups'
+        }
+    });
 };
