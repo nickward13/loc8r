d2b105fb8f13e6d74727d25b9d172a2b2eba1753
Nick Ward <nickward@microsoft.com>
2017-05-04 19:24:01 +1000

    Moved data from about view to controller

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET about page */
 module.exports.about = function(req, res){
-    res.render('generic-text', { title: 'About' });
+    res.render('generic-text', {
+            title: 'About',
+            content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan.'
+        });
 };
